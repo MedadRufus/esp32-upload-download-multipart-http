32df6920e0290e9779a6472ab51798e847cd51e4
MedadRufus <medadrufus@gmail.com>
2021-09-15 16:09:42 +0100

    hardcode stuff

--- UDHttp/src/UDHttp.cpp
+++ UDHttp/src/UDHttp.cpp
@@ -7,7 +7,7 @@
 #include <WiFiClientSecure.h>
 
 #define HEADER "POST %s HTTP/1.1\r\n" \
-                "Host: %s:%d\r\n"\
+                "Host: %s\r\n"\
                 "Connection: close\r\n"\
                 "User-Agent: Arduino/1.0\r\n"\
                 "Accept: */*\r\n"\
@@ -28,37 +28,8 @@ UDHttp::~UDHttp(){
 }
 //simple url parser
 int UDHttp::simpleUrlParser(char *url, char *host, int &port){
-    char *tmp = strstr(url, "http://");
     port = 80;
-    char cport[6];
-    char *tmp2;
-    char *tmp3;
-    if(tmp != NULL && host != NULL){
-        tmp2 = tmp + strlen("http://");
-        tmp = strchr(tmp2, '/');
-        if(tmp != NULL){
-            tmp3 = strchr(tmp2, ':');
-            if(tmp3 == NULL) {
-                int len = tmp - tmp2;
-                if(len < HOST_LEN){
-                    memcpy(host, tmp2, len);
-                    return 0;
-                }
-                printf("increase HOST_LEN\n");
-            } else {
-                int len = tmp3 - tmp2;
-                if(len < HOST_LEN){
-                    memcpy(host, tmp2, len);
-                    memset(cport, 0, 6);
-                    memcpy(cport, tmp3+1, tmp- (tmp3+1));
-                    port = atoi(cport);
-                    return 0;
-                }
-                printf("increase HOST_LEN\n");
-            }
-        }
-    }
-    return -1;
+    host = "beta.medadnewman.co.uk";
 }
 
 void UDHttp::sendChunk(Client *client, uint8_t *buf, int len){
@@ -123,7 +94,7 @@ int UDHttp::upload(char *uploadUrlHandler, char *fileName, int sizeOfFile, DataC
 	contentLen = contentLen + strlen(buf) + sizeOfFile;
     //fill header
     memset(buf, 0, HEADER_SIZE);
-	snprintf(buf, HEADER_SIZE, HEADER, uploadUrlHandler, host, port, contentLen, key);
+	snprintf(buf, HEADER_SIZE, HEADER, uploadUrlHandler, host, contentLen, key);
 	
 	if (!client.connect(host, port)) {
         printf("Connection failed\n");
